76dfa88f3ce1ef6830e008cb43e14dcbe2bead35
Ayush Singh <78557460+Ayush-singh-07@users.noreply.github.com>
2023-02-18 12:18:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const https = require('https');
-const path = require('path')
 
 const app = express();
-app.use(express.static('Public'));
+app.use(express.static('Public'))
 app.use(bodyParser.urlencoded({extended: true}))
 //req.send can be only one time (but we can write to the response using res.write)
 
